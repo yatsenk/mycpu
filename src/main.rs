e12ceff4176014f4b565d7c59ff80ec01d6b109e
Dmytro Yatsenko <132939290+yatsenk@users.noreply.github.com>
2026-04-04 12:04:21 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,41 @@ use crossterm::event::{self, KeyCode};
 use ratatui::layout::{Constraint, Layout, Rect};
 use ratatui::style::{Modifier, Color, Style};
 use ratatui::text::{Span, Line};
-use ratatui::widgets::{Dataset, Axis, Block, Tabs, Chart};
+use ratatui::widgets::{Paragraph, Dataset, Axis, Block, Tabs, Chart, Wrap, ListState, ListItem, List};
 use ratatui::{DefaultTerminal, Frame};
+
+const LOGS: [(&str, &str); 26] = [
+    ("Event1", "INFO"),
+    ("Event2", "INFO"),
+    ("Event3", "CRITICAL"),
+    ("Event4", "ERROR"),
+    ("Event5", "INFO"),
+    ("Event6", "INFO"),
+    ("Event7", "WARNING"),
+    ("Event8", "INFO"),
+    ("Event9", "INFO"),
+    ("Event10", "INFO"),
+    ("Event11", "CRITICAL"),
+    ("Event12", "INFO"),
+    ("Event13", "INFO"),
+    ("Event14", "INFO"),
+    ("Event15", "INFO"),
+    ("Event16", "INFO"),
+    ("Event17", "ERROR"),
+    ("Event18", "ERROR"),
+    ("Event19", "INFO"),
+    ("Event20", "INFO"),
+    ("Event21", "WARNING"),
+    ("Event22", "INFO"),
+    ("Event23", "INFO"),
+    ("Event24", "WARNING"),
+    ("Event25", "INFO"),
+    ("Event26", "INFO"),
+];
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    let tick_rate = std::time::Duration::from_millis(250);
+    let tick_rate = std::time::Duration::from_millis(1000);
     ratatui::run(|terminal| App::new().run(terminal, tick_rate))
 }
 
@@ -98,6 +127,49 @@ impl Signals {
     }
 }
 
+#[derive(Debug)]
+pub struct StatefulList<T> {
+    pub state: ListState,
+    pub items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    pub fn with_items(items: Vec<T>) -> Self {
+        Self {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+}
+
 #[derive(Debug, Default)]
 struct TabsState<'a> {
     titles: Vec<&'a str>,
@@ -128,6 +200,7 @@ struct App<'a> {
     tabs: TabsState<'a>,
     sparkline: Signal<RandomSignal>,
     signals: Signals,
+    logs: StatefulList<(&'a str, &'a str)>,
 }
 
 impl<'a> App<'a> {
@@ -137,7 +210,7 @@ impl<'a> App<'a> {
         let mut sin_signal = SinSignal::new(0.2, 3.0, 18.0);
         let sin1_points = sin_signal.by_ref().take(100).collect();
         Self {
-            title: "TITLE",
+            title: "MyCPU",
             tabs: TabsState::new(vec!["Power", "Info", "Other"]),
             sparkline: Signal {
                 source: rand_signal,
@@ -152,6 +225,7 @@ impl<'a> App<'a> {
                 },
                 window: [0.0, 20.0],
             },
+            logs: StatefulList::with_items(LOGS.to_vec()),
         }
     }
 
@@ -185,7 +259,7 @@ impl<'a> App<'a> {
         }
     }
 
-    fn render(&self, frame: &mut Frame) { 
+    fn render(&mut self, frame: &mut Frame) { 
         let chunks = Layout::vertical([Constraint::Length(3), Constraint::Min(0)]).split(frame.area());
         let tabs = self
             .tabs
@@ -193,7 +267,7 @@ impl<'a> App<'a> {
             .iter()
             .map(|t| Line::from(Span::styled(*t, Style::default().fg(Color::White))))
             .collect::<Tabs>()
-            .block(Block::bordered().style(Color::Black).title(self.title))
+            .block(Block::bordered().style(Color::White).title(self.title))
             .highlight_style(Style::default().fg(Color::LightBlue))
             .select(self.tabs.index);
         frame.render_widget(tabs, chunks[0]);
@@ -203,44 +277,61 @@ impl<'a> App<'a> {
         };
     }
 
-    fn render_first_tab(&self, frame: &mut Frame, area: Rect) {
-        let [h1, h2] = Layout::horizontal([
-            Constraint::Percentage(60),
-            Constraint::Percentage(40),
+    fn render_first_tab(&mut self, frame: &mut Frame, area: Rect) {
+        let chunks = Layout::horizontal([
+            Constraint::Length(70),
+            Constraint::Min(5),
+            Constraint::Length(20),
         ])
-        .areas(area);
+        .split(area);
 
-        let [cpu_usage, gpu_usage] = Layout::vertical([
-            Constraint::Percentage(50),
-            Constraint::Percentage(50),
-        ])
-        .areas(h1);
-
-        let [disk_usage, wifi_usage] = Layout::vertical([
-            Constraint::Percentage(50),
-            Constraint::Percentage(50),
-        ])
-        .areas(h2);
-
-        self.draw_cpu_chart(frame, cpu_usage);
-        self.draw_gpu_chart(frame, gpu_usage);
+        self.draw_chart(frame, chunks[0]);
+        self.draw_text(frame, chunks[1]);
+        self.draw_logs(frame, chunks[2]);
     }
 
-    fn draw_cpu_chart(&self, frame: &mut Frame, area: Rect) {
+    fn draw_logs(&mut self, frame: &mut Frame, area: Rect) {
+        let info_style = Style::default().fg(Color::Blue);
+        let warning_style = Style::default().fg(Color::Yellow);
+        let error_style = Style::default().fg(Color::Magenta);
+        let critical_style = Style::default().fg(Color::Red);
+        let logs: Vec<ListItem> = self
+            .logs
+            .items
+            .iter()
+            .map(|&(evt, level)| {
+                let s = match level {
+                    "ERROR" => error_style,
+                    "CRITICAL" => critical_style,
+                    "WARNING" => warning_style,
+                    _ => info_style,
+                };
+                let content = vec![Line::from(vec![
+                    Span::styled(format!("{level:<9}"), s),
+                    Span::raw(evt),
+                ])];
+                ListItem::new(content)
+            })
+            .collect();
+        let logs = List::new(logs).block(Block::bordered().title("List"));
+        frame.render_stateful_widget(logs, area, &mut self.logs.state);
+    }
+
+    fn draw_chart(&self, frame: &mut Frame, area: Rect) {
         let datasets = vec![
             Dataset::default()
                 .marker(ratatui::symbols::Marker::Dot)
-                .style(Style::default().fg(Color::Cyan))
+                .style(Style::default().fg(Color::Green))
                 .data(&self.signals.sin.points),
         ];
         let chart = Chart::new(datasets)
             .block(
                 Block::bordered().title(Span::styled(
-                    "CPU",
+                    "Usage",
                     Style::default()
-                        .fg(Color::Cyan)
-                        .add_modifier(Modifier::BOLD),
-                )),
+                        .fg(Color::Magenta)
+                        .add_modifier(Modifier::default()),
+                )).style(Color::White),
             )
             .x_axis(
                 Axis::default()
@@ -260,46 +351,33 @@ impl<'a> App<'a> {
         frame.render_widget(chart, area);
     }
 
-    fn draw_gpu_chart(&self, frame: &mut Frame, area: Rect) {
-        let datasets = vec![
-            Dataset::default()
-                .marker(ratatui::symbols::Marker::Dot)
-                .style(Style::default().fg(Color::Red))
-                .data(&self.signals.sin.points),
+    fn draw_text(&self, frame: &mut Frame, area: Rect) {
+        let text = vec![
+            Line::from(vec![
+                Span::from("Temperature: "),
+                Span::styled(format!("75°C"), Style::default().fg(Color::Rgb(255, 165, 0))),
+            ]),
         ];
-        let chart = Chart::new(datasets)
-            .block(
-                Block::bordered().title(Span::styled(
-                    "GPU",
-                    Style::default()
-                        .fg(Color::Cyan)
-                        .add_modifier(Modifier::BOLD),
-                )),
-            )
-            .x_axis(
-                Axis::default()
-                    .style(Style::default().fg(Color::Gray))
-                    .bounds(self.signals.window)
-            )
-            .y_axis(
-                Axis::default()
-                    .style(Style::default().fg(Color::Gray))
-                    .bounds([-20.0, 20.0])
-                    .labels([
-                        Span::styled("0", Style::default().add_modifier(Modifier::BOLD)),
-                        Span::styled("100", Style::default().add_modifier(Modifier::BOLD)),
-                    ]),
-            );
-        frame.render_widget(chart, area);
+        let block = Block::bordered().title(Span::styled(
+            "Overview",
+            Style::default()
+                .fg(Color::Magenta)
+                .add_modifier(Modifier::BOLD),
+        ));
+        let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
+        frame.render_widget(paragraph, area);
     }
 
     fn on_tick(&mut self) {
         self.sparkline.on_tick();
         self.signals.on_tick();
+
+        let log = self.logs.items.pop().unwrap();
+        self.logs.items.insert(0, log);
     }
 }
 
-fn get_cpu_usage() -> Result<f32> {
+fn _get_cpu_usage() -> Result<f32> {
     let mut sys = System::new_with_specifics(
         RefreshKind::nothing().with_cpu(CpuRefreshKind::everything()),
     );
