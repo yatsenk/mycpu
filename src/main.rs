76e26bfe774471286333a1628bdd4ce56b99f7ac
Dmytro Yatsenko <132939290+yatsenk@users.noreply.github.com>
2026-04-08 16:52:22 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use sysinfo::{System, CpuRefreshKind, RefreshKind};
-use rand::distr::{Distribution, Uniform};
-use rand::rngs::ThreadRng;
 
 use color_eyre::Result;
 use crossterm::event::{self, KeyCode};
 use ratatui::layout::{Constraint, Layout, Rect};
-use ratatui::style::{Modifier, Color, Style};
+use ratatui::style::{Color, Style};
 use ratatui::text::{Span, Line};
-use ratatui::widgets::{Paragraph, Dataset, Axis, Block, Tabs, Chart, Wrap, ListState, ListItem, List};
+use ratatui::widgets::{Paragraph, Block, Tabs, Wrap, ListState, ListItem, List, LineGauge};
 use ratatui::{DefaultTerminal, Frame};
+use ratatui::symbols;
 
 const LOGS: [(&str, &str); 26] = [
     ("Event1", "INFO"),
@@ -43,88 +42,6 @@ fn main() -> Result<()> {
     color_eyre::install()?;
     let tick_rate = std::time::Duration::from_millis(1000);
     ratatui::run(|terminal| App::new().run(terminal, tick_rate))
-}
-
-#[derive(Clone, Debug)]
-struct RandomSignal {
-    distribution: Uniform<u64>,
-    rng: ThreadRng,
-}
-
-impl RandomSignal {
-    fn new(lower: u64, upper: u64) -> Self {
-        Self {
-            distribution: Uniform::new(lower, upper).expect("invalid range"),
-            rng: rand::rng(),
-        }
-    }
-}
-
-impl Iterator for RandomSignal {
-    type Item = u64;
-    fn next(&mut self) -> Option<u64> {
-        Some(self.distribution.sample(&mut self.rng))
-    }
-}
-
-#[derive(Clone, Debug)]
-struct SinSignal {
-    x: f64,
-    interval: f64,
-    period: f64,
-    scale: f64,
-}
-
-impl SinSignal {
-    const fn new(interval: f64, period: f64, scale: f64) -> Self {
-        Self {
-            x: 0.0,
-            interval,
-            period,
-            scale,
-        }
-    }
-}
-
-impl Iterator for SinSignal {
-    type Item = (f64, f64);
-    fn next(&mut self) -> Option<Self::Item> {
-        let point = (self.x, (self.x * 1.0 / self.period).sin() * self.scale);
-        self.x += self.interval;
-        Some(point)
-    }
-}
-
-#[derive(Debug)]
-struct Signal<S: Iterator> {
-    source: S,
-    points: Vec<S::Item>,
-    tick_rate: usize,
-}
-
-impl<S> Signal<S>
-where
-    S: Iterator,
-{
-    fn on_tick(&mut self) {
-        self.points.drain(0..self.tick_rate);
-        self.points
-            .extend(self.source.by_ref().take(self.tick_rate));
-    }
-}
-
-#[derive(Debug)]
-struct Signals {
-    sin: Signal<SinSignal>,
-    window: [f64; 2],
-}
-
-impl Signals {
-    fn on_tick(&mut self) {
-        self.sin.on_tick();
-        self.window[0] += 1.0;
-        self.window[1] += 1.0;
-    }
 }
 
 #[derive(Debug)]
@@ -195,37 +112,65 @@ impl<'a> TabsState<'a> {
 }
 
 #[derive(Debug)]
+struct Cpu {
+    sys: System,
+    total_frequency: u64,
+}
+
+impl Cpu {
+    fn new() -> Self {
+        let sys = System::new_with_specifics(
+            RefreshKind::nothing().with_cpu(CpuRefreshKind::everything()),
+        );
+
+        Self {
+            sys,
+            total_frequency: 0,
+        }
+    }
+
+    fn get_usage(&mut self) -> f32 {
+        std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+
+        self.sys.refresh_cpu_all();  
+
+        self.sys.global_cpu_usage()
+    }
+
+    fn get_frequency(&mut self) -> u64 {
+        self.sys.refresh_cpu_all(); 
+
+        self.total_frequency = 0;
+        for cpu in self.sys.cpus() {
+            self.total_frequency += cpu.frequency()
+        }
+
+        self.total_frequency
+    }
+}
+
+
+#[derive(Debug)]
 struct App<'a> {
     title: &'a str,
     tabs: TabsState<'a>,
-    sparkline: Signal<RandomSignal>,
-    signals: Signals,
     logs: StatefulList<(&'a str, &'a str)>,
+    cpu: Cpu,
+    usage: f32,
+    frequency: u64,
+    temperature: f32,
 }
 
 impl<'a> App<'a> {
     fn new() -> Self {
-        let mut rand_signal = RandomSignal::new(0, 100);
-        let sparkline_points = rand_signal.by_ref().take(300).collect();
-        let mut sin_signal = SinSignal::new(0.2, 3.0, 18.0);
-        let sin1_points = sin_signal.by_ref().take(100).collect();
         Self {
             title: "MyCPU",
             tabs: TabsState::new(vec!["Power", "Info", "Other"]),
-            sparkline: Signal {
-                source: rand_signal,
-                points: sparkline_points,
-                tick_rate: 1,
-            },
-            signals: Signals {
-                sin: Signal {
-                    source: sin_signal,
-                    points: sin1_points,
-                    tick_rate: 5,
-                },
-                window: [0.0, 20.0],
-            },
             logs: StatefulList::with_items(LOGS.to_vec()),
+            cpu: Cpu::new(),
+            usage: 0.0,
+            frequency: 0,
+            temperature: 0.0,
         }
     }
 
@@ -258,7 +203,7 @@ impl<'a> App<'a> {
             }
         }
     }
-
+    
     fn render(&mut self, frame: &mut Frame) { 
         let chunks = Layout::vertical([Constraint::Length(3), Constraint::Min(0)]).split(frame.area());
         let tabs = self
@@ -285,7 +230,7 @@ impl<'a> App<'a> {
         ])
         .split(area);
 
-        self.draw_chart(frame, chunks[0]);
+        self.draw_gauge(frame, chunks[0]);
         self.draw_text(frame, chunks[1]);
         self.draw_logs(frame, chunks[2]);
     }
@@ -293,8 +238,8 @@ impl<'a> App<'a> {
     fn draw_logs(&mut self, frame: &mut Frame, area: Rect) {
         let info_style = Style::default().fg(Color::Blue);
         let warning_style = Style::default().fg(Color::Yellow);
-        let error_style = Style::default().fg(Color::Magenta);
-        let critical_style = Style::default().fg(Color::Red);
+        let error_style = Style::default().fg(Color::Red);
+        let critical_style = Style::default().fg(Color::Green);
         let logs: Vec<ListItem> = self
             .logs
             .items
@@ -313,82 +258,57 @@ impl<'a> App<'a> {
                 ListItem::new(content)
             })
             .collect();
-        let logs = List::new(logs).block(Block::bordered().title("List"));
+        let logs = List::new(logs).block(Block::bordered());
         frame.render_stateful_widget(logs, area, &mut self.logs.state);
     }
 
-    fn draw_chart(&self, frame: &mut Frame, area: Rect) {
-        let datasets = vec![
-            Dataset::default()
-                .marker(ratatui::symbols::Marker::Dot)
-                .style(Style::default().fg(Color::Green))
-                .data(&self.signals.sin.points),
-        ];
-        let chart = Chart::new(datasets)
-            .block(
-                Block::bordered().title(Span::styled(
-                    "Usage",
-                    Style::default()
-                        .fg(Color::Magenta)
-                        .add_modifier(Modifier::default()),
-                )).style(Color::White),
-            )
-            .x_axis(
-                Axis::default()
-                    .style(Style::default().fg(Color::Gray))
-                    .bounds(self.signals.window)
-            )
-            .y_axis(
-                Axis::default()
-                    .style(Style::default().fg(Color::Gray))
-                    .bounds([-20.0, 20.0])
-                    .labels([
-                        Span::styled("0", Style::default().add_modifier(Modifier::BOLD)),
-                        Span::raw("50"),
-                        Span::styled("100", Style::default().add_modifier(Modifier::BOLD)),
-                    ]),
-            );
-        frame.render_widget(chart, area);
+    fn draw_gauge(&mut self, frame: &mut Frame, area: Rect) {
+        let chunks = Layout::vertical([
+            Constraint::Length(1),
+            Constraint::Length(3),
+            Constraint::Length(2),
+        ])
+        .horizontal_margin(1)
+        .split(area);
+
+        let block = Block::bordered();
+        frame.render_widget(block, area);
+        
+        let usage_gauge = LineGauge::default()
+            .block(Block::new().title("Usage:"))
+            .filled_style(Style::default().fg(Color::Magenta))
+            .filled_symbol(symbols::line::THICK_HORIZONTAL)
+            .unfilled_symbol(symbols::line::THICK_HORIZONTAL)
+            .label(format!("{}%", self.usage))
+            .ratio(self.usage as f64 / 100.0);
+        frame.render_widget(usage_gauge, chunks[1]);
+
+        let frequency_gauge = LineGauge::default()
+            .block(Block::new().title("Frequency:"))
+            .filled_style(Style::default().fg(Color::Magenta))
+            .filled_symbol(symbols::line::THICK_HORIZONTAL)
+            .unfilled_symbol(symbols::line::THICK_HORIZONTAL)
+            .label(format!("{} MHz", self.frequency))
+            .ratio(self.frequency as f64 / 100_000.0);
+        frame.render_widget(frequency_gauge, chunks[2]);
     }
 
     fn draw_text(&self, frame: &mut Frame, area: Rect) {
         let text = vec![
             Line::from(vec![
                 Span::from("Temperature: "),
-                Span::styled(format!("75°C"), Style::default().fg(Color::Rgb(255, 165, 0))),
+                Span::styled(format!("63°C"), Style::default().fg(Color::Rgb(255, 165, 0))),
             ]),
         ];
-        let block = Block::bordered().title(Span::styled(
-            "Overview",
-            Style::default()
-                .fg(Color::Magenta)
-                .add_modifier(Modifier::BOLD),
-        ));
-        let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
+        let paragraph = Paragraph::new(text).block(Block::bordered()).wrap(Wrap { trim: true });
         frame.render_widget(paragraph, area);
     }
 
     fn on_tick(&mut self) {
-        self.sparkline.on_tick();
-        self.signals.on_tick();
+        self.usage = self.cpu.get_usage();
+        self.frequency = self.cpu.get_frequency();
 
         let log = self.logs.items.pop().unwrap();
         self.logs.items.insert(0, log);
     }
-}
-
-fn _get_cpu_usage() -> Result<f32> {
-    let mut sys = System::new_with_specifics(
-        RefreshKind::nothing().with_cpu(CpuRefreshKind::everything()),
-    );
-
-    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
-
-    sys.refresh_cpu_all();
-
-    for cpu in sys.cpus() {
-        println!("CPU: {}% Usage", cpu.cpu_usage());
-    }
-
-    Ok(sys.global_cpu_usage())
 }
